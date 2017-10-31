7c9f32d1d4359f16e70322d153760fc0204eeaa1
Jeremias Antequera <jereantequera@gmai.com>
2017-10-31 17:05:43 -0300

    Haciendo que diferencie entre un post y un get

--- Onp.ino
+++ Onp.ino
@@ -6,12 +6,21 @@ byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
 IPAddress ip(168, 83, 20, 187);
 EthernetServer server(80);
 EthernetClient client;
+String reaString = String(100); //string for fetching data from address
 
 bool readRequest(EthernetClient& client) {
   bool currentLineIsBlank = true;
   while (client.connected()) {
     if (client.available()) {
       char c = client.read();
+      if (reaString.length() < 100) {
+       //store characters to string 
+       reaString.concat(c);
+      }
+      
+      if(reaString.startsWith("POST /dame")) {
+        return false;
+      }
       if (c == '\n' && currentLineIsBlank) {
         return true;
       } else if (c == '\n') {
@@ -46,27 +55,26 @@ void setup() {
   
   
 }
+const int arraysize = 10;
 const char* denominacion[] = {"Vivero La Piedra", "Vivero Fenix"};
 const char* empresa[] = {"La Piedra srl", "Fenix S.A"};
 const char* clave[] = {"23418544321", "43418544123"};
 const char* direccion[] = { "Piedra Buena 420", "Las Piedras 240" };
 const float valores[] = { 4.20, 2.40, 4, 5};
-const char* unidades[] = { "C", "lux", "%", "m3" };
+const char* unidades[arraysize] = { "c", "so", "%", "m3","r1", "r2", "pr1", "pr2", "z", "s"};
 const char* duracion[] = { "4:20", "2:40" };
 JsonObject& createJson () {
-  StaticJsonBuffer<512> jsonBuffer;
+  StaticJsonBuffer<1000> jsonBuffer;
   JsonObject& root = jsonBuffer.createObject();
-  JsonObject& vivero = root.createNestedObject("vivero");
-  vivero["Denominacion"] = denominacion[random(0,2)];
-  vivero["Empresa"] = empresa[random(0,2)];
-  vivero["ClaveFiscal"] = clave[random(0,2)];
-  vivero["Direccion"] = direccion[random(0,2)];
-  JsonObject& medicion = root.createNestedObject("medicion");
-  medicion["valor"] = valores[random(0,4)];
-  medicion["UnidadFisica"] = unidades[random(0,4)];
-  medicion["Estado_Transductor"] = random(1,4);
-  medicion["Duracion"] = duracion[random(0,2)];
-  medicion["Estado_Transmicion"] = random(1,3);
+  JsonObject& vivero = root.createNestedObject("v");
+  vivero["c"] = clave[random(0,2)];
+  for (int unidad = 0; unidad < arraysize; unidad++){
+  JsonObject& medicion = root.createNestedObject(unidades[unidad]);
+        medicion["v"] = valores[random(0,4)];
+        medicion["er"] = random(1,4);
+        medicion["d"] = duracion[random(0,2)];
+        medicion["en"] = random(1,3);
+  }     
   return (root);
 }
 
@@ -76,30 +84,15 @@ void loop() {
     Serial.println("New Client");
     bool success = readRequest(client);
     if (success) {
+      Serial.println(success);
+      Serial.println(reaString);
       writeResponse(client, createJson());
+    }else{
+      Serial.println("holanda");
     }
     delay(1);
     client.stop();
   }
-    Serial.println("client disconnected");
+    
   }
-
-void postJson(JsonObject& json) {
-    if (client.connect("168.83.20.187",80)) { // REPLACE WITH YOUR SERVER ADDRESS
-    client.println("POST / HTTP/1.1"); 
-    client.println("Host: 168.83.20.187"); // SERVER ADDRESS HERE TOO
-    client.println("Content-Type: application/json"); 
-    client.print("Content-Length: "); 
-    client.println(json.measureLength()); 
-    client.println(); 
-    json.prettyPrintTo(client);
-  } 
-
-  if (client.connected()) { 
-    client.stop();  // DISCONNECT FROM THE SERVER
-  }
-}
-
-
-
 
