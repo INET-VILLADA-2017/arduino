a0880281b73bcadaf5ed9acf4598923354e02157
Jeremias Antequera <jereantequera@gmai.com>
2017-11-01 12:06:12 -0300

    Finished and working

--- Onp.ino
+++ Onp.ino
@@ -1,12 +1,17 @@
 #include <ArduinoJson.h>
 #include <Ethernet.h>
 #include <SPI.h>
+#include <EEPROM.h>
+
 
 byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
 IPAddress ip(168, 83, 20, 187);
 EthernetServer server(80);
 EthernetClient client;
 String reaString = String(100); //string for fetching data from address
+String toSave = String(25);
+int addr = 0;
+byte byteArray[25];
 
 bool readRequest(EthernetClient& client) {
   bool currentLineIsBlank = true;
@@ -14,12 +19,7 @@ bool readRequest(EthernetClient& client) {
     if (client.available()) {
       char c = client.read();
       if (reaString.length() < 100) {
-       //store characters to string 
-       reaString.concat(c);
-      }
-      
-      if(reaString.startsWith("POST /dame")) {
-        return false;
+        reaString.concat(c);
       }
       if (c == '\n' && currentLineIsBlank) {
         return true;
@@ -52,8 +52,8 @@ void setup() {
   server.begin();
   Serial.print("server is at ");
   Serial.println(Ethernet.localIP());
-  
-  
+
+
 }
 const int arraysize = 10;
 const char* denominacion[] = {"Vivero La Piedra", "Vivero Fenix"};
@@ -61,20 +61,20 @@ const char* empresa[] = {"La Piedra srl", "Fenix S.A"};
 const char* clave[] = {"23418544321", "43418544123"};
 const char* direccion[] = { "Piedra Buena 420", "Las Piedras 240" };
 const float valores[] = { 4.20, 2.40, 4, 5};
-const char* unidades[arraysize] = { "c", "so", "%", "m3","r1", "r2", "pr1", "pr2", "z", "s"};
+const char* unidades[arraysize] = { "c", "so", "%", "m3", "r1", "r2", "pr1", "pr2", "z", "s"};
 const char* duracion[] = { "4:20", "2:40" };
 JsonObject& createJson () {
   StaticJsonBuffer<1000> jsonBuffer;
   JsonObject& root = jsonBuffer.createObject();
   JsonObject& vivero = root.createNestedObject("v");
-  vivero["c"] = clave[random(0,2)];
-  for (int unidad = 0; unidad < arraysize; unidad++){
-  JsonObject& medicion = root.createNestedObject(unidades[unidad]);
-        medicion["v"] = valores[random(0,4)];
-        medicion["er"] = random(1,4);
-        medicion["d"] = duracion[random(0,2)];
-        medicion["en"] = random(1,3);
-  }     
+  vivero["c"] = clave[random(0, 2)];
+  for (int unidad = 0; unidad < arraysize; unidad++) {
+    JsonObject& medicion = root.createNestedObject(unidades[unidad]);
+    medicion["v"] = valores[random(0, 4)];
+    medicion["er"] = random(1, 4);
+    medicion["d"] = duracion[random(0, 2)];
+    medicion["en"] = random(1, 3);
+  }
   return (root);
 }
 
@@ -83,16 +83,48 @@ void loop() {
   if (client) {
     Serial.println("New Client");
     bool success = readRequest(client);
-    if (success) {
+    if (success && reaString.startsWith("POST")) {
+      Serial.println("POST");
+      Serial.println(reaString);
+      toSave = reaString.substring(7, 32);
+      Serial.println(toSave);
+      saveString(toSave);
+      toSave  = "";
+      reaString = "";
+      readMemory();
+      client.println("HTTP/1.1 200 OK");
+      client.println("Content-Type: text/html");
+      client.println();
+    } else {
       Serial.println(success);
       Serial.println(reaString);
+      reaString = "";
       writeResponse(client, createJson());
-    }else{
-      Serial.println("holanda");
     }
     delay(1);
     client.stop();
   }
-    
+}
+
+void saveString(String toSave) {
+  Serial.println(toSave.length());
+  for ( addr = 0; addr <= toSave.length(); addr++) {
+    if (addr == EEPROM.length()) {
+      addr = 0;
+    }
+    Serial.println(toSave[addr]);
+    EEPROM.write(addr, toSave[addr]);
   }
+}
+
+String readMemory(){
+    String storedValues;
+    for ( addr = 0; addr <= 25; addr++) {
+      byteArray[addr] = EEPROM.read(addr);
+  }
+  storedValues = String((char *)byteArray);
+  Serial.println(storedValues);
+  return storedValues;
+}
+
 
